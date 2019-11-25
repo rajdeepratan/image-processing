1312469d21f80ae7fe657922636442422baca4cb
Rajdeep Ratan <rajdeepratan@hotmail.com>
2019-11-25 22:36:02 +0530

    fix image height and error

--- src/Container/ImageOps.jsx
+++ src/Container/ImageOps.jsx
@@ -4,12 +4,11 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 
-import { Image, Video, Transformation, CloudinaryContext } from 'cloudinary-react';
+import { Image, Transformation } from 'cloudinary-react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 
@@ -83,9 +82,9 @@ export default class ImageOpsContainer extends React.Component {
             return transform.value;
         }
 
-        if (type == "rgb") {
+        if (type === "rgb") {
             return this.getRBBCons().find((transform) => transform.value === key).default;
-        } else if (type == "hsv") {
+        } else if (type === "hsv") {
             return this.getHSVCons().find((transform) => transform.value === key).default;
         }
 
@@ -151,9 +150,9 @@ export default class ImageOpsContainer extends React.Component {
         let transforms = this.state.transforms;
 
         // transforms = this.getUpdatedTransform(transforms, hue);
-        if (type == "grayscale") {
+        if (type === "grayscale") {
             transforms = this.getUpdatedTransform(transforms, saturation);
-        } else if (type == "sepia") {
+        } else if (type === "sepia") {
             transforms = this.getUpdatedTransform(transforms, hue);
             transforms = this.getUpdatedTransform(transforms, saturation);
         }
